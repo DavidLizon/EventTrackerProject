d14c662a4606cfcac6283f849eff6eee37b887ce
David Lizon <draggero@gmail.com>
2022-01-07 15:40:37 -0700

    Created Spring Boot project

---
+++ PurchaseTracker/src/test/java/com/skilldistillery/purchases/PurchaseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.purchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
