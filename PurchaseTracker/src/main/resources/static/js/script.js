32ab36d433bbe084c1aae67cf7c5cf87e02afe5b
David Lizon <draggero@gmail.com>
2022-01-15 14:49:06 -0700

    Display all purchases in table complete and working

--- PurchaseTracker/src/main/resources/static/js/script.js
+++ PurchaseTracker/src/main/resources/static/js/script.js
@@ -11,7 +11,6 @@ function init() {
 	//TODO - setup event listeners for forms, etc.
 }
 
-
 function getListPurchases() {
 
 	let xhr = new XMLHttpRequest();
@@ -23,7 +22,7 @@ function getListPurchases() {
 			if(xhr.status === 200){
 				let purchases = JSON.parse(xhr.responseText);
 				console.log('in onreadystate')
-				displayPurchases(purchases);
+				displayAllPurchases(purchases);
 			}
 			else if (xhr.status === 404){
 				console.log('Purchases not found.');
@@ -40,54 +39,131 @@ function getListPurchases() {
 // 	purchaseDiv.textContent = msg;
 // };
 
-function displayPurchases(purchases){
-	
+function displayAllPurchases(purchases){
+
 	console.log('in displayPurchases');
 	
 	let purchaseDiv = document.getElementById('purchaseList');
+	purchaseDiv.textContent = 'purchases made: ' + purchases.length;
 	purchaseDiv.textContent = '';
+	
+
+	var table = document.createElement('table');
+	// document.body.appendChild(table);
+	purchaseDiv.appendChild(table);
+
+	// let tBody = document.createElement('tBody');
+	// table.appendChild(tBody);
 
 	for(let purchase of purchases) {
-	let h4 = document.createElement('h4');
-	h4.textContent = `Purchase ${purchase.id}: ${purchase.name}`;
-	purchaseDiv.appendChild(h4);
 
-	let ul = document.createElement('ul');
-	purchaseDiv.appendChild(ul);
+		// table head
+		//  -------------------------------------------------------
+		let tHead = document.createElement('tHead');
+		table.appendChild(tHead);
 
-	let li = document.createElement('li');
-	li.textContent = purchase.purchaseDate; 
-	ul.appendChild(li);
-	
-	
+		let tr = document.createElement('tr');
+		tHead.appendChild(tr);
 
-	// for(let element in purchase) {
+		let th = document.createElement('th');
+		th.id = 'purchaseDateTable';
+		th.textContent = purchase.purchaseDate;
+		tHead.appendChild(th);
 
-	// 	displaySinglePurchase(element);
+		//  -------------------------------------------------------
+		let tBody = document.createElement('tBody');
+		tBody.addEventListener('click', function(e){
+			e.preventDefault();
+			getPurchase(purchase.id);
+		});
+		table.appendChild(tBody);
 
-	// 	if(element !== 'name' || element !== 'id'){
-	// 	let li = document.createElement('li');
-	// 	li.textContent = purchase[element];	
+		tr = document.createElement('tr');
+		tBody.appendChild(tr);
+
+		// cell with order name
+		td = document.createElement('td');
+		td.id = 'orderNameTable';
+		td.textContent = purchase.name;
+		tBody.appendChild(td)
+
+		// cell with store name
+		td = document.createElement('td');
+		td.id = 'storeNameTable';
+		td.textContent = purchase['store']['name'];
+		tBody.appendChild(td)
 		
-		// if( purchase[index].value === 'store'){
-		// 	for(var store in index){
-		// 		console.log('inside of store object')
-		// 		let li2 = document.createElement('li');
-		// 		li2.textContent = index[store];
-		// 		ul.appendChild(li2);
-		// 	}
-		// }
-		// to handle object do another for in check if p = store or something
+		if(purchase.returned) {
+			td = document.createElement('td');
+			td.id = 'returnedTable';
+			td.textContent = 'Returned';
+			tBody.appendChild(td)
+		} else {	
+			// if !online display Orfer Comlete, if en route display en route, 
+			// if delivered display Items delivered
+			td = document.createElement('td');
+			td.id = 'statusTable';
+			if(!purchase.online){
+				td.textContent = 'Items picked up'
+			} else if (purchase.arrivalDate != null && purchase.delivered != true){
+				td.textContent = 'Items en route';
+			} else if (purchase.delivered) {
+				td.textContent = 'Delivery completed';
+			}
+			tBody.appendChild(td);
 
-		// let li3 = document.createElement('li');
-		// li3.textContent = purchase[index].store;
-		// ul.appendChild(li3);
-		
-	// 	ul.appendChild(li);
-	// 	}
-	// }
-		//let li = document.createElement('li');
-		//li.textContent = purchase.name;
-		//ul.appendChild(li); 
+			// new row for expected delivery date
+			tr = document.createElement('tr');
+			tBody.appendChild(tr);
+
+			// blank for order name and store to span 2 rows
+			td = document.createElement('td');
+			tBody.appendChild(td)
+			td = document.createElement('td');
+			tBody.appendChild(td)
+			
+
+			// if shipped expected delivery date
+			td = document.createElement('td');
+
+			if(!purchase.online){
+				td.textContent = `Completed: ${purchase.purchaseDate}`;
+			} else if (purchase.arrivalDate != null) {
+				if(purchase.delivered == true){
+					td.textContent = 'Items delivered';
+				} else {
+					td.textContent = `Delivery date: ${purchase.arrivalDate}`;
+				}
+			}
+			tBody.appendChild(td)
+
+			// new row for return date
+			tr = document.createElement('tr');
+			tBody.appendChild(tr);
+
+			// blank for order name and store to span 2 rows
+			td = document.createElement('td');
+			tBody.appendChild(td)
+			td = document.createElement('td');
+			tBody.appendChild(td)
+			
+			// cell with return date, calculate return date based on arrival date
+			td = document.createElement('td');
+			if(purchase['returnDate'] == null) {
+				console.log('typeof: ' + typeof purchase['purchaseDate'] )
+				let date = new Date(purchase['purchaseDate']);
+				let daysToAdd = purchase['store']['numberDaysCanReturnPurchase'];
+				let newDate = new Date(new Date().setDate(date.getDate() + daysToAdd ));
+				td.textContent = `Return By: ${newDate}`;
+			} else {
+				td.textContent = `Return By: ${purchase['returnDate']}`;
+			}
+			tBody.appendChild(td)
+		}
 	}
+}
+
+function getPurchase(purchaseId) {
+	
+
 }
